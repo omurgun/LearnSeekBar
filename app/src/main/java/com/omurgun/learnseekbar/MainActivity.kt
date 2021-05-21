6c71cc5a0d24f050af2de540500607a49ae76488
ömür <omur_gun@outlook.com>
2021-05-21 19:41:50 +0300

    Initial commit

---
+++ app/src/main/java/com/omurgun/learnseekbar/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.omurgun.learnseekbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
